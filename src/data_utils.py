d49bf17897d42bec6b9b004427d397c794c367f3
stefanoribes <ribes.stefano@gmail.com>
2023-10-24 17:57:16 +0200

    Fixed load_dataset bug

--- src/data_utils.py
+++ src/data_utils.py
@@ -2,6 +2,7 @@ import pandas as pd
 import os
 from datasets import load_dataset
 from transformers import AutoTokenizer
+from typing import Optional
 
 def process_data_to_model_inputs(
     batch,
@@ -28,10 +29,11 @@ def load_tokenized_dataset(
     batch_size: int = 512,
     encoder_max_length:int = 256,
     decoder_max_length:int = 256,
+    token: Optional[str] = None,
 ):
     if isinstance(tokenizer, str):
         tokenizer = AutoTokenizer.from_pretrained(tokenizer)
-    dataset = load_dataset(daset_dir)
+    dataset = load_dataset(daset_dir, token=token)
     dataset_tokenized = dataset.map(
         process_data_to_model_inputs,
         batched=True,
