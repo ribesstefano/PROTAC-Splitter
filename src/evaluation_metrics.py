bc99c7440beeaaee17979178b626ea854a334a5e
stefanoribes <ribes.stefano@gmail.com>
2023-11-02 14:53:13 +0100

    Removed Tanimoto from eval-metric

--- src/evaluation_metrics.py
+++ src/evaluation_metrics.py
@@ -61,16 +61,16 @@ def compute_metrics_with_chem(
     # Get has_all_attachment_points score
     num_attach_points = np.array([has_all_attachment_points(s) for s in pred_str])
     scores['has_all_attachment_points'] = num_attach_points.mean()
-    # Get tanimoto score
-    pred_str = np.array(pred_str)[valid_smiles == 1]
-    label_str = np.array(label_str)[valid_smiles == 1]
-    if len(pred_str) == 0:
-        scores['tanimoto'] = 0.0
-        return scores
-    pred_mols = [Chem.MolFromSmiles(s) for s in pred_str]
-    label_mols = [Chem.MolFromSmiles(s) for s in label_str]
-    pred_fps = [fpgen.GetFingerprint(m) for m in pred_mols]
-    label_fps = [fpgen.GetFingerprint(m) for m in label_mols]
-    tanimoto = [DataStructs.TanimotoSimilarity(l, p) for l, p in zip(label_fps, pred_fps)]
-    scores['tanimoto'] = np.array(tanimoto).mean()
+    # # Get tanimoto score
+    # pred_str = np.array(pred_str)[valid_smiles == 1]
+    # label_str = np.array(label_str)[valid_smiles == 1]
+    # if len(pred_str) == 0:
+    #     scores['tanimoto'] = 0.0
+    #     return scores
+    # pred_mols = [Chem.MolFromSmiles(s) for s in pred_str]
+    # label_mols = [Chem.MolFromSmiles(s) for s in label_str]
+    # pred_fps = [fpgen.GetFingerprint(m) for m in pred_mols]
+    # label_fps = [fpgen.GetFingerprint(m) for m in label_mols]
+    # tanimoto = [DataStructs.TanimotoSimilarity(l, p) for l, p in zip(label_fps, pred_fps)]
+    # scores['tanimoto'] = np.array(tanimoto).mean()
     return scores
