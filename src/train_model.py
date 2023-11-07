040a6fc793e446d9d523f397d4610041d2e1f31e
stefanoribes <ribes.stefano@gmail.com>
2023-11-07 23:01:43 +0100

    Added MLM training

--- src/train_model.py
+++ src/train_model.py
@@ -7,14 +7,19 @@ from transformers import (
     Seq2SeqTrainingArguments,
     EncoderDecoderModel,
     AutoTokenizer,
+    AutoModelForMaskedLM,
+    DataCollatorForLanguageModeling,
+    Trainer,
+    TrainingArguments,
 )
+from datasets import load_dataset
 import evaluate
 from typing import Optional
 from functools import partial
 import huggingface_hub as hf
 from rdkit import Chem, RDLogger
 from rdkit.Chem import rdFingerprintGenerator
-
+import math
 
 def create_hf_repository(**kwargs):
   """Creates a new Hugging Face repository."""
@@ -181,3 +186,116 @@ def train_model(
             dataset=ds_name,
             dataset_args=ds_config,
         )
+
+
+def train_mlm_model(
+    model_name: str,
+    ds_name: str = 'ailab-bio/PROTAC-Substructures',
+    ds_config: str = 'encoder_mlm_dataset',
+    max_steps: int = 2000,
+    num_train_epochs: int = -1,
+    batch_size: int = 128,
+    batch_size_tokenizer: int = 1024,
+    gradient_accumulation_steps: int = 4,
+    hub_token: Optional[str] = None,
+    organization: Optional[str] = None,
+    output_dir: str = "./models/",
+    mlm_probability: float = 0.15,
+    tokenizer: AutoTokenizer | str = "seyonec/ChemBERTa-zinc-base-v1",
+    pretrained_model_name: str = "seyonec/ChemBERTa-zinc-base-v1",
+    max_length: int = 256,
+    delete_repo_first: bool = False,
+):
+    # Setup output directory and Hugging Face repository
+    output_dir += f"/{model_name}"
+    if organization is not None:
+        hub_model_name = f"{organization}/{model_name}"
+        if delete_repo_first:
+            delete_hf_repository(repo_id=hub_model_name, token=hub_token)
+        repo_url = create_hf_repository(
+            repo_id=hub_model_name,
+            repo_type="model",
+            exist_ok=True,
+            private=True,
+            token=hub_token,
+        )
+        print(f"Repository '{hub_model_name}' created at URL: {repo_url}")
+    else:
+        hub_model_name = None
+    # Load pretrained MLM model
+    model = AutoModelForMaskedLM.from_pretrained(pretrained_model_name, max_length=max_length)
+    tokenizer = AutoTokenizer.from_pretrained(pretrained_model_name)
+    # Load tokenizer
+    tokenizer.pad_token = tokenizer.eos_token
+    data_collator = DataCollatorForLanguageModeling(tokenizer=tokenizer, mlm_probability=mlm_probability)
+    # Load and tokenize MLM dataset
+    mlm_dataset = load_dataset(ds_name, ds_config)
+    tokenized_mlm_dataset = mlm_dataset.map(
+        lambda examples: tokenizer(examples["text"]),
+        batched=True,
+        batch_size=batch_size_tokenizer,
+        remove_columns=["text", "labels"],
+    )
+    # Setup trainer
+    per_device_batch_size = batch_size // gradient_accumulation_steps
+    training_args = TrainingArguments(
+        output_dir=output_dir,
+        # Optimizer-related configs
+        learning_rate=5e-5,
+        optim="adamw_torch",
+        lr_scheduler_type="cosine", # Default: "linear"
+        weight_decay=0.01,
+        # Batch size and device configs
+        per_device_train_batch_size=per_device_batch_size,
+        per_device_eval_batch_size=per_device_batch_size,
+        gradient_accumulation_steps=gradient_accumulation_steps,
+        auto_find_batch_size=True,
+        # Evaluation and checkpointing configs
+        evaluation_strategy="steps",
+        max_steps=max_steps,
+        num_train_epochs=num_train_epochs,
+        eval_steps=100,
+        save_steps=500,
+        fp16=True,
+        # Logging configs
+        log_level="info",
+        logging_steps=50,
+        disable_tqdm=False,
+        # Hub information configs
+        push_to_hub=True, # NOTE: Done manually further down
+        hub_token=hub_token,
+        hub_model_id=hub_model_name,
+        hub_strategy="checkpoint", # NOTE: Allows to resume training from last checkpoint 
+        hub_private_repo=True,
+        # Other configs
+        seed=42,
+        data_seed=42,
+    )
+    trainer = Trainer(
+        model=model,
+        tokenizer=tokenizer,
+        args=training_args,
+        train_dataset=tokenized_mlm_dataset["train"],
+        eval_dataset=tokenized_mlm_dataset["validation"].select(range(100)),
+        data_collator=data_collator,
+    )
+    # Get perplexity before training
+    eval_results = trainer.evaluate()
+    print(f"Perplexity before training: {math.exp(eval_results['eval_loss']):.2f}")
+    # Train model
+    trainer.train()
+    # Get perplexity after training
+    eval_results = trainer.evaluate()
+    print(f"Perplexity after training: {math.exp(eval_results['eval_loss']):.2f}")
+    # Push model to Hugging Face Hub
+    # if hub_model_name is not None:
+    #     trainer.push_to_hub(
+    #         commit_message="Initial version",
+    #         model_name=hub_model_name,
+    #         license="mit",
+    #         finetuned_from=pretrained_model_name,
+    #         tasks=["Fill-Mask"],
+    #         tags=["PROTAC", "cheminformatics"],
+    #         dataset=ds_name,
+    #         dataset_args=ds_config,
+    #     )
