ef4a7381518345fe9ae65d4904a2e84a7832e2dc
Stefan+o <ribes.stefano@gmail.com>
2024-12-30 14:03:09 +0100

    Implemented data generation following functional groups distribution.

--- protac_splitter/chemoinformatics.py
+++ protac_splitter/chemoinformatics.py
@@ -1,6 +1,7 @@
 import random
 import logging
 from typing import List, Tuple, Callable, Any, Union, Dict, Optional, Literal
+from multiprocessing import Process, Queue
 
 import numpy as np
 from tqdm import tqdm
@@ -11,6 +12,40 @@ from rdkit.Chem import (
     rdFingerprintGenerator,
     rdMolHash,
 )
+
+def GetSubstructMatchesWithTimeout(
+    mol: Chem.Mol,
+    substruct: Chem.Mol,
+    useChirality: bool = True,
+    maxMatches: int = 50,
+    timeout: int | float = 10,
+) -> Optional[List[List[int]]]:
+    """ Get substructure matches with a timeout.
+
+    Args:
+        mol (Chem.Mol): The molecule to search for substructure matches.
+        substruct (Chem.Mol): The substructure to search for in the molecule.
+        useChirality (bool, optional): Whether to use chirality in the substructure search. Defaults to True.
+        maxMatches (int, optional): The maximum number of matches to return. Defaults to 50.
+        timeout (int | float, optional): The timeout in seconds. Defaults to 10.
+    
+    Returns:
+        Optional[List[List[int]]]: A list of lists containing the atom indices of the substructure matches. Returns None if the search times out or failed.
+    """
+    def worker(q, mol, substruct, useChirality, maxMatches):
+        q.put(list(mol.GetSubstructMatches(substruct, useChirality=useChirality, maxMatches=maxMatches)))
+
+    q = Queue()
+    p = Process(target=worker, args=(q, mol, substruct, useChirality, maxMatches))
+    p.start()
+    p.join(timeout)
+
+    if p.is_alive():
+        p.terminate()
+        p.join()
+        return None
+    else:
+        return q.get()
 
 
 def canonize_smiles(smiles: str, fail_on_error: bool = False) -> str:
