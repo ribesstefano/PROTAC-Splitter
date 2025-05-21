95a5f1aea5b3e2880c575338917c5a9f9e7c742d
ribesstefano <ribes.stefano@gmail.com>
2025-05-21 17:06:22 +0200

    Added script to extract graph features from PROTAC splitter datasets on HF.

---
+++ scripts/get_graph_dataset.py
@@ -0,0 +1,280 @@
+import argparse
+from typing import Tuple, List, Union, Optional
+
+from rdkit import Chem
+from rdkit.Chem import AllChem, Descriptors
+import networkx as nx
+import pandas as pd
+import numpy as np
+from tqdm import tqdm
+from datasets import load_dataset
+
+from protac_splitter.chemoinformatics import get_atom_idx_at_attachment
+
+
+def extract_edge_features(
+    protac_smiles: str,
+    e3_split_pair: Optional[Tuple[int, int]] = None,
+    wh_split_pair: Optional[Tuple[int, int]] = None,
+    n_bits: int = 512,
+    radius: int = 6,
+    descriptor_names: Optional[List[str]] = None,
+    fp_as_string: bool = False,
+) -> pd.DataFrame:
+    """Extract features from the edges of a PROTAC molecule represented as a SMILES string.
+    
+    Parameters:
+        protac_smiles (str): SMILES representation of the PROTAC molecule.
+        e3_split_pair (Tuple[int, int]): Indices of the E3 split pair.
+        wh_split_pair (Tuple[int, int]): Indices of the warhead split pair.
+        n_bits (int): Number of bits for Morgan fingerprints.
+        radius (int): Radius for Morgan fingerprints.
+        descriptor_names (List[str]): List of RDKit descriptor names to compute.
+        
+    Returns:
+        pd.DataFrame: DataFrame containing edge features.
+    """
+    mol = Chem.MolFromSmiles(protac_smiles)
+    if mol is None:
+        raise ValueError(f"Input SMILES could not be parsed: {protac_smiles}")
+    # Canonicalize the SMILES
+    mol = Chem.MolFromSmiles(Chem.MolToSmiles(mol, canonical=True))
+    if mol is None:
+        raise ValueError(f"Input SMILES could not be canonicalized: {protac_smiles}")
+
+    # Step 1: Convert SMILES to NetworkX
+    G = nx.Graph()
+    # for atom in mol.GetAtoms():
+    #     G.add_node(atom.GetIdx())
+    for bond in mol.GetBonds():
+        G.add_edge(bond.GetBeginAtomIdx(), bond.GetEndAtomIdx())
+    
+    num_nodes = G.number_of_nodes()
+    num_edges = G.number_of_edges()
+
+    # Step 2: Create line graph and compute betweenness + degree
+    LG = nx.line_graph(G)
+    line_betweenness = nx.betweenness_centrality(LG, endpoints=True)
+    betweenness = nx.betweenness_centrality(G, endpoints=True)
+
+    # Compute k-hop degrees (number of nodes within 2, 3 hops)
+    # TODO: Shall I get the degree of the node in the line graph or the original graph?
+    line_degree = dict(LG.degree())
+    line_degree_r2 = {}
+    line_degree_r3 = {}
+    for node in LG.nodes():
+        # Nodes within radius 2 and 3 (excluding the center node)
+        neighbors_r2 = nx.single_source_shortest_path_length(LG, node, cutoff=2)
+        neighbors_r3 = nx.single_source_shortest_path_length(LG, node, cutoff=3)
+        line_degree_r2[node] = len([n for n, d in neighbors_r2.items() if d == 2])
+        line_degree_r3[node] = len([n for n, d in neighbors_r3.items() if d == 3])
+
+    degree = dict(G.degree())
+    degree_r2 = {}
+    degree_r3 = {}
+    for node in G.nodes():
+        # Nodes within radius 2 and 3 (excluding the center node)
+        neighbors_r2 = nx.single_source_shortest_path_length(G, node, cutoff=2)
+        neighbors_r3 = nx.single_source_shortest_path_length(G, node, cutoff=3)
+        degree_r2[node] = len([n for n, d in neighbors_r2.items() if d == 2])
+        degree_r3[node] = len([n for n, d in neighbors_r3.items() if d == 3])
+
+    if e3_split_pair is not None and wh_split_pair is not None:
+        true_split_edges = {frozenset(e3_split_pair), frozenset(wh_split_pair)}
+
+    # Get molecular characteristics, i.e., Morgan fingerprints and descriptors
+    # Generate Morgan fingerprint
+    fp_bitvec = AllChem.GetMorganFingerprintAsBitVect(mol, radius, nBits=n_bits)
+    fp = np.zeros((n_bits,), dtype=np.float32)
+    AllChem.DataStructs.ConvertToNumpyArray(fp_bitvec, fp)
+    if fp_as_string:
+        fp = {"chem_mol_fp": "".join([str(int(bit)) for bit in fp])}
+    else:
+        fp = {f"chem_mol_fp_{i}": bool(fp[i]) for i in range(n_bits)}
+    # Generate RDKit descriptors
+    descriptor_func_names = descriptor_names or [
+        "MolWt", "HeavyAtomCount", "NumHAcceptors", "NumHDonors",
+        "TPSA", "NumRotatableBonds", "RingCount", "MolLogP"
+    ]
+    functions = [getattr(Descriptors, name) for name in descriptor_func_names]
+    descriptors = {f"chem_mol_desc_{name}": func(mol) for name, func in zip(descriptor_func_names, functions)}
+
+    # Step 3: Gather edge features
+    # NOTE: Only consider bridge nodes
+    edge_features = []
+    for (u, v) in nx.bridges(G):
+        bond = mol.GetBondBetweenAtoms(u, v)
+
+        # Avoid reporting the same edge twice (i.e., swap u and v if needed) and
+        # ensure to find the node pair in the line graph
+        node = (u, v) if (u, v) in LG else (v, u)
+        node_key = node if node in line_betweenness else (v, u)
+
+        features = {
+            "graph_bond_idx": bond.GetIdx(),
+            "graph_num_nodes": num_nodes,
+            "graph_num_edges": num_edges,
+            "graph_betweenness": line_betweenness.get(node_key, 0.0),
+            "graph_degree": line_degree.get(node_key, 0),
+            "graph_degree_r2": line_degree_r2.get(node_key, 0),
+            "graph_degree_r3": line_degree_r3.get(node_key, 0),
+            "graph_node_u_degree": degree.get(u, 0),
+            "graph_node_u_degree_r2": degree_r2.get(u, 0),
+            "graph_node_u_degree_r3": degree_r3.get(u, 0),
+            "graph_node_v_degree": degree.get(v, 0),
+            "graph_node_v_degree_r2": degree_r2.get(v, 0),
+            "graph_node_v_degree_r3": degree_r3.get(v, 0),
+            "graph_node_u_betweenness": betweenness.get(u, 0.0),
+            "graph_node_v_betweenness": betweenness.get(v, 0.0),
+            "chem_bond_type": str(bond.GetBondType()),
+            "chem_atom_u": mol.GetAtomWithIdx(u).GetSymbol(),
+            "chem_atom_v": mol.GetAtomWithIdx(v).GetSymbol(),
+            "chem_is_aromatic": bond.GetIsAromatic(),
+            "chem_is_in_ring": bond.IsInRing(),
+            "chem_mol_smiles": protac_smiles,
+            "chem_mol_n_bits": n_bits,
+            "chem_mol_radius": radius,
+        }
+        # Add RDKit descriptors and Morgan fingerprint
+        features.update(fp)
+        features.update(descriptors)
+
+        # Add E3 and warhead split labels
+        if e3_split_pair is not None and wh_split_pair is not None:
+            features.update({
+                "label_is_split": frozenset([u, v]) in true_split_edges,
+                "label_e3_split": frozenset([u, v]) == frozenset(e3_split_pair),
+                "label_wh_split": frozenset([u, v]) == frozenset(wh_split_pair),
+            })
+
+        # Append the features to the list of edge features
+        edge_features.append(features)
+
+    df = pd.DataFrame(edge_features)
+
+    # Identify columns with int64 dtype
+    int64_cols = df.select_dtypes(include=['int64']).columns
+
+    # Create a dictionary mapping these columns to int32
+    dtype_mapping = {col: np.int32 for col in int64_cols}
+
+    # Apply the type conversion
+    df = df.astype(dtype_mapping)
+    
+    return df
+
+def get_edge_features(
+    protac_smiles: Union[str, List[str]],
+    wh_smiles: Union[str, List[str]],
+    lk_smiles: Union[str, List[str]],
+    e3_smiles: Union[str, List[str]],
+    n_bits: int = 512,
+    radius: int = 6,
+    descriptor_names: Optional[List[str]] = None,
+    fp_as_string: bool = False,
+    verbose: int = 0,
+) -> pd.DataFrame:
+    """Get edge features for a given PROTAC molecule and its components.
+    
+    Parameters:
+        protac_smiles (str | List[str]): SMILES representation of the PROTAC molecule.
+        wh_smiles (str | List[str]): SMILES representation of the warhead.
+        lk_smiles (str | List[str]): SMILES representation of the linker.
+        e3_smiles (str | List[str]): SMILES representation of the E3 binder.
+        n_bits (int): Number of bits for Morgan fingerprints.
+        radius (int): Radius for Morgan fingerprints.
+        descriptor_names (List[str]): List of RDKit descriptor names to compute.
+        
+    Returns:
+        pd.DataFrame: DataFrame containing edge features.
+    """
+    if isinstance(protac_smiles, str):
+        protac_smiles = [protac_smiles]
+    if isinstance(wh_smiles, str):
+        wh_smiles = [wh_smiles]
+    if isinstance(lk_smiles, str):
+        lk_smiles = [lk_smiles]
+    if isinstance(e3_smiles, str):
+        e3_smiles = [e3_smiles]
+
+    iterables = zip(protac_smiles, wh_smiles, lk_smiles, e3_smiles)
+    iterables = tqdm(iterables, desc="Extracting edge features", total=len(protac_smiles), disable=verbose == 0)
+    features_list = []
+    for protac_smi, wh_smi, lk_smi, e3_smi in iterables:
+        # Convert SMILES to RDKit molecules
+        protac = Chem.MolFromSmiles(protac_smi)
+        wh = Chem.MolFromSmiles(wh_smi)
+        lk = Chem.MolFromSmiles(lk_smi)
+        e3 = Chem.MolFromSmiles(e3_smi)
+        if protac is None or wh is None or lk is None or e3 is None:
+            raise ValueError(f"Invalid SMILES string: {protac}, {wh}, {lk}, {e3}")
+
+        # Get the attachment points
+        wh_edge = get_atom_idx_at_attachment(protac, wh, lk)
+        e3_edge = get_atom_idx_at_attachment(protac, e3, lk)
+
+        # Extract features
+        features = extract_edge_features(
+            protac_smi,
+            e3_split_pair=e3_edge,
+            wh_split_pair=wh_edge,
+            n_bits=n_bits,
+            radius=radius,
+            descriptor_names=descriptor_names,
+            fp_as_string=fp_as_string,
+        )
+
+        # Append the features to the list
+        features_list.append(features)
+
+    return pd.concat(features_list, ignore_index=True)
+
+
+def get_substructs(row):
+    text = row["text"]
+    labels = row["labels"]
+    return {
+        "PROTAC SMILES": text,
+        "POI Ligand SMILES with direction": labels.split(".")[2],
+        "Linker SMILES with direction": labels.split(".")[1],
+        "E3 Binder SMILES with direction": labels.split(".")[0],
+    }
+
+def main(
+    num_proc: int = 8,
+    hf_token: Optional[str] = None,
+):
+    # Load the dataset
+    ds = load_dataset("ailab-bio/PROTAC-Splitter-Dataset", "clustered")
+    ds = ds.map(get_substructs, num_proc=num_proc, remove_columns=["text", "labels"])
+
+    ds = ds.map(
+        lambda x: get_edge_features(
+            protac_smiles=x["PROTAC SMILES"],
+            wh_smiles=x["POI Ligand SMILES with direction"],
+            lk_smiles=x["Linker SMILES with direction"],
+            e3_smiles=x["E3 Binder SMILES with direction"],
+            n_bits=512,
+            radius=6,
+            verbose=0,
+        ),
+        num_proc=num_proc,
+        batched=True,
+        remove_columns=["PROTAC SMILES", "POI Ligand SMILES with direction", "Linker SMILES with direction", "E3 Binder SMILES with direction"],
+    )
+
+    # Upload the dataset to Hugging Face
+    ds.push_to_hub(
+        "ailab-bio/PROTAC-Splitter-Graph-Dataset",
+        token=hf_token,
+        private=True,
+    )
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Extract edge features from PROTAC molecules.")
+    parser.add_argument("--num_proc", type=int, default=8, help="Number of processes to use for parallel processing.")
+    parser.add_argument("--hf_token", type=str, help="Hugging Face token for uploading the dataset.")
+    args = parser.parse_args()
+
+    main(num_proc=args.num_proc, hf_token=args.hf_token)
